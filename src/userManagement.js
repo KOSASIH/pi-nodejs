1a87a15438587568d59fb0418c83531dae65db21
KOSASIH <kosasihg88@gmail.com>
2025-01-07 22:42:55 +0700

    Create userManagement.js

---
+++ src/userManagement.js
@@ -0,0 +1,17 @@
+// src/userManagement.js
+
+class UserManagement {
+    createUser(userData) {
+        // Logic to create a new user
+    }
+
+    updateUser(userId, updatedData) {
+        // Logic to update user information
+    }
+
+    getUser(userId) {
+        // Logic to retrieve user information
+    }
+}
+
+export default new UserManagement();
