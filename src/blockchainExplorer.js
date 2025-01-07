1053256e4fbdd3669e6d9bd523635beffeb55e77
KOSASIH <kosasihg88@gmail.com>
2025-01-07 22:49:57 +0700

    Create blockchainExplorer.js

---
+++ src/blockchainExplorer.js
@@ -0,0 +1,17 @@
+// src/blockchainExplorer.js
+
+class BlockchainExplorer {
+    getTransaction(transactionId) {
+        // Logic to retrieve transaction details
+    }
+
+    getBlock(blockNumber) {
+        // Logic to retrieve block details
+    }
+
+    getLatestBlocks(limit) {
+        // Logic to retrieve the latest blocks
+    }
+}
+
+export default new BlockchainExplorer();
