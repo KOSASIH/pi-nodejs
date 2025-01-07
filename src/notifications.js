eba57e6c8a8eaa8c5eaea375fa8dfdc39f9d5981
KOSASIH <kosasihg88@gmail.com>
2025-01-07 22:52:24 +0700

    Create notifications.js

---
+++ src/notifications.js
@@ -0,0 +1,17 @@
+// src/notifications.js
+
+class Notifications {
+    sendEmailNotification(email, message) {
+        // Logic to send email notifications
+    }
+
+    sendPushNotification(userId, message) {
+        // Logic to send push notifications
+    }
+
+    getUser Notifications(userId) {
+        // Logic to retrieve notifications for a user
+    }
+}
+
+export default new Notifications();
