22037f0d5b6526040446702e94a7e17ae3f0d4af
KOSASIH <kosasihg88@gmail.com>
2025-01-10 15:16:15 +0700

    Create websocket.js

---
+++ src/utils/websocket.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+    
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+    });
+
+    ws.send('Welcome to the Pi Network WebSocket server!');
+});
+
+const broadcast = (data) => {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(data));
+        }
+    });
+};
+
+module.exports = { broadcast };
